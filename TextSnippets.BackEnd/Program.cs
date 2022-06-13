398b449fd1fb476daec78530e7073044aeb0003f
Hong Pham Huu <hongph85@gmail.com>
2022-06-14 00:24:21 +0700

    Handling errors

--- TextSnippets.BackEnd/Program.cs
+++ TextSnippets.BackEnd/Program.cs
@@ -21,6 +21,36 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+    app.UseStatusCodePages(Text.Plain, "Status Code Page: {0}");
+}
+else
+{
+    app.UseStatusCodePages();
+    app.UseExceptionHandler(exceptionHandlerApp =>
+    {
+        exceptionHandlerApp.Run(async context =>
+        {
+            context.Response.StatusCode = StatusCodes.Status500InternalServerError;
+
+            // using static System.Net.Mime.MediaTypeNames;
+            context.Response.ContentType = Text.Plain;
+
+            await context.Response.WriteAsync("An exception was thrown.");
+
+            var exceptionHandlerPathFeature =
+                context.Features.Get<IExceptionHandlerPathFeature>();
+
+            if (exceptionHandlerPathFeature?.Error is FileNotFoundException)
+            {
+                await context.Response.WriteAsync(" The file was not found.");
+            }
+
+            if (exceptionHandlerPathFeature?.Path == "/")
+            {
+                await context.Response.WriteAsync(" Page: Home.");
+            }
+        });
+    });
 }
 
 app.UseHttpsRedirection();
